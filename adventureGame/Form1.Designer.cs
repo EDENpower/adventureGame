968499bf07cc381e291541c2b24cd05bc98fa974
P4 <P4@TK_018>
2021-11-11 22:17:21 -0500

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace adventureGame
+{
+    partial class gameBackground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gameBackground));
+            this.iconBox = new System.Windows.Forms.PictureBox();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.beginButton = new System.Windows.Forms.Button();
+            this.wizardImageBox = new System.Windows.Forms.PictureBox();
+            this.nextFormButton = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.Button();
+            this.option2 = new System.Windows.Forms.Button();
+            this.gameText = new System.Windows.Forms.Label();
+            this.slimeImage = new System.Windows.Forms.PictureBox();
+            this.fightImage = new System.Windows.Forms.PictureBox();
+            this.damselImage = new System.Windows.Forms.PictureBox();
+            this.angryElfImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slimeImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fightImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.damselImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angryElfImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iconBox
+            // 
+            this.iconBox.BackColor = System.Drawing.Color.Transparent;
+            this.iconBox.Image = ((System.Drawing.Image)(resources.GetObject("iconBox.Image")));
+            this.iconBox.Location = new System.Drawing.Point(264, 60);
+            this.iconBox.Name = "iconBox";
+            this.iconBox.Size = new System.Drawing.Size(239, 168);
+            this.iconBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconBox.TabIndex = 0;
+            this.iconBox.TabStop = false;
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.welcomeLabel.Font = new System.Drawing.Font("NSimSun", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.Color.White;
+            this.welcomeLabel.Location = new System.Drawing.Point(230, 251);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(46, 21);
+            this.welcomeLabel.TabIndex = 1;
+            this.welcomeLabel.Text = "...";
+            // 
+            // beginButton
+            // 
+            this.beginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.beginButton.FlatAppearance.BorderSize = 0;
+            this.beginButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.beginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.beginButton.Font = new System.Drawing.Font("NSimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginButton.ForeColor = System.Drawing.Color.White;
+            this.beginButton.Location = new System.Drawing.Point(326, 317);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(74, 39);
+            this.beginButton.TabIndex = 2;
+            this.beginButton.Text = "BEGIN!";
+            this.beginButton.UseVisualStyleBackColor = false;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            // 
+            // wizardImageBox
+            // 
+            this.wizardImageBox.BackColor = System.Drawing.Color.Transparent;
+            this.wizardImageBox.Image = ((System.Drawing.Image)(resources.GetObject("wizardImageBox.Image")));
+            this.wizardImageBox.Location = new System.Drawing.Point(326, 234);
+            this.wizardImageBox.Name = "wizardImageBox";
+            this.wizardImageBox.Size = new System.Drawing.Size(137, 256);
+            this.wizardImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.wizardImageBox.TabIndex = 3;
+            this.wizardImageBox.TabStop = false;
+            // 
+            // nextFormButton
+            // 
+            this.nextFormButton.Font = new System.Drawing.Font("NSimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextFormButton.ForeColor = System.Drawing.Color.White;
+            this.nextFormButton.Location = new System.Drawing.Point(693, 394);
+            this.nextFormButton.Name = "nextFormButton";
+            this.nextFormButton.Size = new System.Drawing.Size(95, 47);
+            this.nextFormButton.TabIndex = 6;
+            this.nextFormButton.Text = "CLICK TO CONTINUE ";
+            this.nextFormButton.Click += new System.EventHandler(this.nextFormButton_Click);
+            // 
+            // option1
+            // 
+            this.option1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.option1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option1.Font = new System.Drawing.Font("NSimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1.ForeColor = System.Drawing.Color.White;
+            this.option1.Location = new System.Drawing.Point(117, 172);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(141, 56);
+            this.option1.TabIndex = 7;
+            this.option1.Text = "YES";
+            this.option1.UseVisualStyleBackColor = true;
+            this.option1.Click += new System.EventHandler(this.option1_Click);
+            // 
+            // option2
+            // 
+            this.option2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.option2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option2.Font = new System.Drawing.Font("NSimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2.ForeColor = System.Drawing.Color.White;
+            this.option2.Location = new System.Drawing.Point(117, 251);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(141, 56);
+            this.option2.TabIndex = 8;
+            this.option2.Text = "NO";
+            this.option2.UseVisualStyleBackColor = true;
+            this.option2.Click += new System.EventHandler(this.option2_Click);
+            // 
+            // gameText
+            // 
+            this.gameText.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gameText.Font = new System.Drawing.Font("NSimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameText.ForeColor = System.Drawing.Color.White;
+            this.gameText.Location = new System.Drawing.Point(117, 9);
+            this.gameText.Name = "gameText";
+            this.gameText.Size = new System.Drawing.Size(525, 103);
+            this.gameText.TabIndex = 9;
+            this.gameText.Text = "A young wizard is off on his journey after his graduation from the magic  academy" +
+    ", he is ready to begin his grand journey.";
+            // 
+            // slimeImage
+            // 
+            this.slimeImage.BackColor = System.Drawing.Color.Transparent;
+            this.slimeImage.Image = ((System.Drawing.Image)(resources.GetObject("slimeImage.Image")));
+            this.slimeImage.Location = new System.Drawing.Point(509, 327);
+            this.slimeImage.Name = "slimeImage";
+            this.slimeImage.Size = new System.Drawing.Size(120, 191);
+            this.slimeImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.slimeImage.TabIndex = 10;
+            this.slimeImage.TabStop = false;
+            // 
+            // fightImage
+            // 
+            this.fightImage.BackColor = System.Drawing.Color.Transparent;
+            this.fightImage.Image = ((System.Drawing.Image)(resources.GetObject("fightImage.Image")));
+            this.fightImage.Location = new System.Drawing.Point(12, 115);
+            this.fightImage.Name = "fightImage";
+            this.fightImage.Size = new System.Drawing.Size(776, 341);
+            this.fightImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fightImage.TabIndex = 11;
+            this.fightImage.TabStop = false;
+            // 
+            // damselImage
+            // 
+            this.damselImage.BackColor = System.Drawing.Color.Transparent;
+            this.damselImage.Image = ((System.Drawing.Image)(resources.GetObject("damselImage.Image")));
+            this.damselImage.Location = new System.Drawing.Point(618, 234);
+            this.damselImage.Name = "damselImage";
+            this.damselImage.Size = new System.Drawing.Size(170, 296);
+            this.damselImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.damselImage.TabIndex = 12;
+            this.damselImage.TabStop = false;
+            // 
+            // angryElfImage
+            // 
+            this.angryElfImage.BackColor = System.Drawing.Color.Transparent;
+            this.angryElfImage.Image = ((System.Drawing.Image)(resources.GetObject("angryElfImage.Image")));
+            this.angryElfImage.Location = new System.Drawing.Point(435, 152);
+            this.angryElfImage.Name = "angryElfImage";
+            this.angryElfImage.Size = new System.Drawing.Size(228, 304);
+            this.angryElfImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.angryElfImage.TabIndex = 13;
+            this.angryElfImage.TabStop = false;
+            // 
+            // gameBackground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.angryElfImage);
+            this.Controls.Add(this.damselImage);
+            this.Controls.Add(this.fightImage);
+            this.Controls.Add(this.slimeImage);
+            this.Controls.Add(this.gameText);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.nextFormButton);
+            this.Controls.Add(this.wizardImageBox);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.iconBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "gameBackground";
+            this.Text = "A WIZARDS ADVENTURE";
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slimeImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fightImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.damselImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angryElfImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox iconBox;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Button beginButton;
+        private System.Windows.Forms.PictureBox wizardImageBox;
+        private System.Windows.Forms.Label nextFormButton;
+        private System.Windows.Forms.Button option1;
+        private System.Windows.Forms.Button option2;
+        private System.Windows.Forms.Label gameText;
+        private System.Windows.Forms.PictureBox slimeImage;
+        private System.Windows.Forms.PictureBox fightImage;
+        private System.Windows.Forms.PictureBox damselImage;
+        private System.Windows.Forms.PictureBox angryElfImage;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace adventureGame
+{
+    public partial class gameBackground : Form
+    {
+
+
+        //Music input
+        SoundPlayer beginmusicplayer = new SoundPlayer(Properties.Resources.begin_music);
+        SoundPlayer wizardline1 = new SoundPlayer(Properties.Resources.Wizard_line_1);
+        SoundPlayer wizardline2 = new SoundPlayer(Properties.Resources.Wizard_line_2);
+        SoundPlayer Damsel = new SoundPlayer(Properties.Resources.Damsel);
+        SoundPlayer AngryElf = new SoundPlayer(Properties.Resources.AngryElf);
+        //integers 
+        int page = 1;
+
+        public gameBackground()
+        {
+            
+            InitializeComponent();
+
+            //Hide
+            welcomeLabel.Visible = false;
+            wizardImageBox.Visible = false;
+            gameText.Visible = false;
+            nextFormButton.Visible = false;
+            option1.Visible = false;
+            option2.Visible = false;
+            slimeImage.Visible = false;
+            fightImage.Visible = false;
+            angryElfImage.Visible = false;
+            damselImage.Visible = false;
+
+        }
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            beginmusicplayer.Play();
+
+            //welcome message
+            welcomeLabel.Visible = true;
+            welcomeLabel.Text = $"AN EDEN POWER PRODUCTION";
+
+            //Stop music
+            Refresh();
+            Thread.Sleep(2000);
+
+            //disable labels 
+            iconBox.Visible = false;
+            welcomeLabel.Visible = false;
+            beginButton.Visible = false;
+
+            //Show images
+            wizardImageBox.Visible = true;
+            gameText.Visible = true;
+            nextFormButton.Visible = true;
+
+            //music
+            beginmusicplayer.Play();
+        }
+
+        private void nextFormButton_Click(object sender, EventArgs e)
+        {
+            //Disable Images
+            wizardImageBox.Visible = false;
+            nextFormButton.Visible = false;
+
+            //Enable Images
+            option1.Visible = true;
+            option2.Visible = true;
+
+            //Text Display
+            gameText.Text = "Are you ready to begin young Wizard?";
+
+
+
+        }
+
+        private void option2_Click(object sender, EventArgs e)
+        {
+            if (page == 1) { page = 99; }
+            else if (page == 4) { page = 5; }
+            else if (page == 5) { page = 99; }
+            else if (page == 6) { page = 8; }
+            else if (page == 8) { page = 1; }
+            else if (page == 5) { page = 1; }
+            else if (page == 11) { page = 14; }
+
+            DisplayPage();
+
+        }
+
+        private void option1_Click(object sender, EventArgs e)
+        {
+            if (page == 1) { page = 4; }
+            else if (page == 4) { page = 6; }
+            else if (page == 6) { page = 7; }
+            else if (page == 7) { page = 10; }
+            else if (page == 10) { page = 11; }
+            else if (page == 11) { page = 1; }
+           
+
+            DisplayPage();
+
+
+        }
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    beginmusicplayer.Play();
+                    //welcome message
+                    welcomeLabel.Visible = true;
+                    welcomeLabel.Text = $"AN EDEN POWER PRODUCTION";
+
+                    //Stop music
+                    Refresh();
+                    Thread.Sleep(2000);
+
+                    //disable labels 
+                    iconBox.Visible = false;
+                    welcomeLabel.Visible = false;
+                    beginButton.Visible = false;
+                    fightImage.Visible = false;
+                    option1.Visible = false;
+                    option2.Visible = false;
+                    gameText.Visible = false;
+
+                    //Show images
+                    wizardImageBox.Visible = true;
+                    gameText.Visible = true;
+                    nextFormButton.Visible = true;
+
+                    //music
+                    beginmusicplayer.Play();
+                    break;
+                case 4:
+                    //coordinates 
+                    //wizardImageBox.Scale = (366, 234);
+
+                    //Disable Labels
+                    option1.Visible = false;
+                    option2.Visible = false;
+
+                    //Show Images
+                    slimeImage.Visible = false;
+                    wizardImageBox.Visible = true;
+
+                    // Text and chacater sound output
+                    gameText.Text = "The Wizard sets off on his journey when suddenly...";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    wizardline1.Play();
+                    gameText.Text = "Wizard:";
+                    gameText.Text += $"\nGAH!A SLIME";
+                    slimeImage.Visible = true;
+
+                    //Move to next page
+                    Refresh();
+                    Thread.Sleep(2000);
+                    wizardline1.Stop();
+                    wizardImageBox.Visible = false;
+                    slimeImage.Visible = false;
+                    gameText.Text = "Will you fight the slime?";
+                    Refresh();
+                    Thread.Sleep(1000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+
+                    break;
+                case 5:
+                    gameText.Text = "You couldn't escape and you died :(";
+                    fightImage.Visible = true;
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Would you like to play again?";
+                    option2.Visible = true;
+                    option1.Visible = true;
+                    break;
+                case 6:
+                    //Show Image
+                    fightImage.Visible = true;
+
+                    //disable images
+                    option1.Visible = false;
+                    option2.Visible = false;
+
+                    //text output
+                    gameText.Text = "he faces the slime head on as he begins to chant a magic spell";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    wizardline2.Play();
+                    gameText.Text = "Wizard";
+                    gameText.Text += "\nLet's do this!";
+                    wizardline2.Stop();
+                    gameText.Text = "Sparks fly from his hands as he strikes the slime";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "The slime disintegrates in an instant and the wizard is able to continue on his journey";
+
+                    //next form
+                    Refresh();
+                    Thread.Sleep(3000);
+                    fightImage.Visible = false;
+                    wizardImageBox.Visible = true;
+                    gameText.Text = "As you continue on with your journey you come to a fork in the road!!";
+                    Refresh();
+                    Thread.Sleep(1000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+                    option1.Text = "RIGHT";
+                    option2.Text = "LEFT";
+
+                    break;
+                case 7:
+                    // disable options
+                    option1.Visible = false;
+                    option2.Visible = false;
+
+                    gameText.Text = "You find a damsel...OH NO! It seems shes in distress";
+                    Refresh();
+                    Thread.Sleep(2000);
+
+                    // Show Characters
+                    angryElfImage.Visible = true;
+                    damselImage.Visible = true;
+
+                    Refresh();
+                    Thread.Sleep(2000);
+
+                    // character sounds
+                    Damsel.Play();
+                    gameText.Text = "Damsel:";
+                    gameText.Text += $"\nHELP, HEEE- !!";
+                    Refresh();
+                    Thread.Sleep(2000);
+
+                    AngryElf.Play();
+                    gameText.Text = "Angry Elf:";
+                    gameText.Text += $"\nShut it LADY!!";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Will you help her?";
+                    option2.Visible = true;
+                    option1.Visible = true;
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 8:
+                    gameText.Text = "Oh no!...You got eaten by giant spiders :(";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Play again...?";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+                    break;
+
+                case 9:
+                    //disable buttons
+                    option2.Visible = false;
+                    option1.Visible = false;
+                    gameText.Text = "You trip over a bigroot while trying to get away and you die";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Play again...?";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+                    break;
+
+                case 10:
+                    //disable characters
+                    angryElfImage.Visible = false;
+
+                    //disable buttons
+                    option1.Visible = false;
+                    option2.Visible = false;
+                    gameText.Text = "You saved her!! She comes in close...";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Do you kiss her?";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+                    break;
+
+                case 11:
+                    option1.Visible = false;
+                    option2.Visible = false;
+
+                    gameText.Text = "You both fall madly in love and continue on you great adventure to become an all powerful wizard.";
+                    Refresh();
+                    Thread.Sleep(3000);
+
+                    gameText.Text += $"THANKS FOR PLAYING MY GAME :) \n Thanks too: Kay Broda, Ali Doupe, Soaphie Hoffman, Mr.Osborne, Eric Evans, Ella Ruby, and Violet ShipWay, and Ewan Siebert!!!";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Play again...?";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+                    break;
+
+                case 12:
+                    gameText.Text = "she leaves you forever and you live with the mistake for the rest of your life :C ";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    gameText.Text = "Play again...?";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    option1.Visible = true;
+                    option2.Visible = true;
+                    break;
+
+                case 13:
+                    gameText.Text = "you overcome the great temptation of love and move forward on your adventure you become a great powerful wizard !!";
+                    break;
+
+                case 99:
+                    //Text Display
+                    gameText.Text = "Thanks for playing";
+                    //Closing Form
+                    Refresh();
+                    Thread.Sleep(1000);
+                    this.Close();
+                    break;
+                default:
+                    break;
+
+               
+                  
+            }
+        }
+
+      
+    }
+}
